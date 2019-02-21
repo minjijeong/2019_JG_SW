b70bc0f8b3b925e205b53809934872b555551e90
earthK <k94earth@naver.com>
2019-02-22 00:51:43 +0900

    modification of code

--- boj_16234.cpp
+++ boj_16234.cpp
@@ -15,29 +15,23 @@ int dy[4] = { -1,1,0,0 };
 
 queue<pair<int, int>> q;
 
-void DFS(pair<int, int> v)	{
-	int x, y, r, c, sub;
-
-	x = v.first;
-	y = v.second;
+void DFS(int x, int y) {
+	int r, c, sub;
 
 	if (visited[x][y] == true)
-		return ;
+		return;
 
 	// 방문체크, num 증가, 인구수 누적, 큐에 enqueue
 	visited[x][y] = true;
 	num++;
 	sum += A[x][y];
-	q.push(v);
+	q.push(make_pair(x, y));
 
 	//이동 가능한 좌표가 있으면 w에 저장 후, 현재 좌표를 스택에 push한다.
 	for (int i = 0; i < 4; i++)
 	{
-		x = v.first;
-		y = v.second;
-		
-		r = x + dy[i];	
-		c = y + dx[i]; 
+		r = x + dy[i];
+		c = y + dx[i];
 
 		if (!(0 <= r && 0 <= c && r < N && c < N))
 			continue;
@@ -47,7 +41,7 @@ void DFS(pair<int, int> v)	{
 		if (sub < 0) sub = -sub;
 
 		if ((L <= sub && sub <= R) && (visited[r][c] == false)) {
-			DFS(make_pair(r, c));
+			DFS(r, c);
 		}
 	}
 }
@@ -61,26 +55,25 @@ int main() {
 		}
 	}
 
-	sum = num = val = 0;
-
-	while(true) {
+	while (true) {
 		int groupNum = 0;
+
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				visited[i][j] = false;
 			}
 		}
-		//DFS로 방문체크를 하면서 sum,num을 구한다.
+
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				sum = num = val = 0;
 
-				DFS(make_pair(i, j));
+				DFS(i, j);
 
 				if (num > 1) {
+					// 연합을 이루는 경우
 					val = sum / num;
 
-					// 방문 했던 모든 좌표의 값을 val로 변경
 					while (!q.empty())
 					{
 						int x, y;
@@ -94,15 +87,15 @@ int main() {
 						//연합 개수 카운트
 						groupNum++;
 					}
-
 				}
-				else if (num == 1){
+				else if (num == 1) {
 					// 하나만 방문한 경우 다시 초기화 해야 한다.
 					q.pop();
 					visited[i][j] = false;
 				}
 			}
 		}
+
 		if (groupNum == 0)
 			break;
 		else
